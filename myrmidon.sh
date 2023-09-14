4133629fd55a49afcaf1095c96f07f0bde3b8808
Lennart <hoff.lenn@gmail.com>
2023-09-14 15:25:09 +0200

    clean up and starting to refactore

--- myrmidon.sh
+++ myrmidon.sh
@@ -1,52 +1,32 @@
 #!/bin/bash
-cwd=$(echo $(dirname $0))
-
 # Use ~/.myrmidon-tasks.json as default, otherwise use incoming path
-config_file="${1:-"$PWD/uni-tasks.json"}"
-tasks=$(cat $config_file)
-
-
-# Pass tasks to rofi, and get the output as the selected option
-selected=$(echo $tasks | jq -j 'map(.name) | join("\n")' | rofi -dmenu -matching fuzzy -i -p "Search tasks")
-task=$(echo $tasks | jq -j ".[] | select(.name == \"$selected\")")
-
-# Exit if no task was found
-if [[ $task == "" ]]; then
-  echo "No task defined as '$selected' within config file."
-  exit 1
-fi
-
-command_name=$(echo $task | jq -j ".name")
-task_command=$(echo $task | jq -j ".command")
-confirm=$(echo $task | jq ".confirm")
-task_bool=$(echo $task | jq ".tasks")
-
-if [[ $task_bool == "true" ]]; then
-  # Chain the confirm command before executing the selected command
-
-  # get the task you want to execute
-  tasks_file="$HOME/.config/i3/uni-tasks.json"
-  timer_tasks=$(cat $tasks_file)
-
-  #Pass tasks to rofi, and get the output as the selected option
-  selected=$(echo $timer_tasks | jq -j 'map(.name) | join("\n")' | rofi -dmenu -matching fuzzy -i -p "Which tasks")
-  timer_task=$(echo $timer_tasks | jq ".[] | select(.name == \"$selected\")")
-
-  if [[ $timer_task == "" ]]; then
-    echo "No task defined as '$selected' within config file."
-    exit 1
+checkIfTogglIsRunning (){
+  response=$(toggl now)
+  if [[ $response == "There is no time entry running!" ]]; then
+    echo "no timer running"
   fi
+}
 
-  task_name=$(echo $timer_task | jq -j ".name")
-  echo $task_name
-  # get the the project
-  project_file="$HOME/.config/i3/uni-projects.json"
-  projects=$(cat $project_file)
-  selected=$(echo $projects | jq -j 'map(.name) | join("\n")' | rofi -dmenu -matching fuzzy -i -p "which project?")
-  project=$(echo $projects | jq ".[] | select(.name == \"$selected\")")
+getProjectsInformation (){
+  echo "$(toggl --no-header projects ls)" 
+}
 
-  project_name=$(echo $project | jq -j ".name")
-  if [[ $project == "" ]]; then
+createRofiPopUp ()
+{
+  local popUpText="$1"
+  local popUpSelection="$2"
+  selected=$(echo "$popUpSelection" | awk '{print}' | paste -sd '\n' - | rofi -dmenu -matching fuzzy -i -p "$popUpText")
+  echo "$selected"
+}
+echo "$(getProjectsInformation)"
+echo "$(checkIfTogglIsRunning)"
+popUpText="select task"
+returnValue="$(createRofiPopUp "$popUpText" "$(getProjectsInformation)")"
+echo "$returnValue"
+
+
+
+if [[ $project == "" ]]; then
     eval "$task_command $task_name"
     dunstify "$(toggl now)"
 
@@ -55,7 +35,6 @@ if [[ $task_bool == "true" ]]; then
     $toggl_now = eval "toggl now"
     dunstify "$(toggl now)"
   fi
-fi
 
 echo $command_name
 if [[ $command_name == "Stop-Timer" ]]; then
